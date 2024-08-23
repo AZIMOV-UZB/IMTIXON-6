186c8ce083ab121f3bdfda2084fe27eb23cc79aa
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-08-22 20:47:56 -0700

    react

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,58 @@
-import React from 'react'
-import Hero from '../../components/hero/Hero'
-import Shops from '../../components/shops/Shops'
-import Pro from '../../components/pro/Pro'
-import Banner from '../../components/banner/Banner'
+import Hero from "@/components/hero/Hero";
+import Skeleton from "@/components/skeleton/skeleton";
+import { useFetch } from "../../hooks/useFetch";
+import React, { useState } from "react";
+import Shops from "@/components/shops/Shops";
+import Pro from "@/components/pro/Pro";
+import Banner from "@/components/banner/Banner";
+import Products from "@/components/products/products";
 
 const Home = () => {
-  return (
-    <div>
-        <Hero/>
-        <Shops/>
-        <Pro/>
-        <Banner/>
-    </div>
-  )
-}
+  const [category, setCategory] = useState("");
+  const { data, loading } = useFetch(
+    `/products${category ? `/category/${category}` : ""}`,
+    { limit: 10 },
+    [category]
+  );
+  const { data: categories } = useFetch("/products/category-list");
 
-export default Home
+  const products = (
+    <div className="container py-4 mt-10">
+      <ul className="flex whitespace-nowrap overflow-x-auto scroll-none gap-4">
+        <li>
+          <button
+            onClick={() => setCategory("")}
+            className="px-4 py-1 bg-green-500 rounded-3xl text-white"
+          >
+            All
+          </button>
+        </li>
+        {categories?.map((cat) => (
+          <li key={cat}>
+            <button
+              onClick={() => setCategory(cat)}
+              className="px-4 py-1 bg-green-500 rounded-3xl text-white"
+            >
+              {cat.split("-").join(" ")}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {!loading ? <Products data={data?.products} /> : <Skeleton />}
+    </div>
+  );
+  return (
+    <>
+   <div>
+<Hero/>
+<Shops/>
+{products}
+<Pro/>
+<Banner/>
+</div>
+   
+    </>
+  );
+};
+
+export default Home;

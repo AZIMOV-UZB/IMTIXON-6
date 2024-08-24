a339508b5b589fe7767dc3e4cbf6e186c69c792e
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-08-24 09:52:30 -0700

    react

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Wishlist from './pages/wishlist/Wishlist'
 import Login from './pages/login/Login'
 import Not from './components/not-found/Not'
 import Shop from './components/shop/Shop'
+import Admin from './pages/admin/Admin'
+import Prof from "./pages/admin/Profil";
+import User from "./pages/admin/User";
+import Auth from './components/auth/Auth'
 
 const App = () => {
   return (
@@ -18,8 +22,15 @@ const App = () => {
         <Route path="wishlist" element={<Wishlist/>}/>
         <Route path="login" element={<Login/>}/>
         <Route path="*" element={<Not/>}/>
+        <Route path="/" element={<Auth/>} >
+        <Route path="/Admin/*" element={<Admin/>}>
+          <Route path="profil" element={<Prof/>} />
+          <Route path="user" element={<User/>} />
+        </Route>
+        </Route>
         </Route>
       </Routes>
+
     </>
     </div>
   )
--- src/components/royxat/Royxat.jsx
+++ src/components/royxat/Royxat.jsx
@@ -1,10 +1,11 @@
+import { NavLink } from "react-router-dom";
 import images from "../../assets/Header.svg";
 const Royxar = () => {
   return (
-    <div className="continer mt-5 mb-12">
-      <div className="w-[330px] sm:w-[400px] md:w-[400px] lg:w-[400px] border h-[403px] rounded-[5px] m-auto p-[15px] sm:p-[31px] md:p-[31px] lg:p-[31px] flex flex-col gap-[25.87px]">
+    <div className="continer mt-5 mb-16">
+      <div className="w-[330px] sm:w-[400px] md:w-[400px] lg:w-[400px] border h-[480px] rounded-[5px] m-auto p-[15px] sm:p-[31px] md:p-[31px] lg:p-[31px] flex flex-col gap-[25.87px]">
         <div className="w-full">
-          <img className="flex m-auto" src={images} alt="" />
+          <img className="flex m-auto" src={images} alt="foto" />
         </div>
         <div className="flex flex-col text-start">
           <label className="text-[15px] text-[#323131] dark:text-white" htmlFor="">
@@ -20,7 +21,7 @@ const Royxar = () => {
           <label className="text-[15px] text-[#323131] dark:text-white"  htmlFor="">
             Password*
           </label>
-          <input
+          <input required
             className="w-full border py-[10px] pl-1 rounded-[5px] outline-none text-[14px] dark:text-black"
             type="text"
             placeholder="Enter Your password"
@@ -39,6 +40,14 @@ const Royxar = () => {
           </button>
           <p className="text-[16px] text-[#323131] dark:text-white">Signup?</p>
         </div>
+        
+          <div className="block">
+        <NavLink className={""} to={"/Admin"}>
+          <button className="w-full h-[40px] text-[14px] text-[#fff] bg-[#3BB77E] rounded-[5px]">
+            Admin
+          </button>
+          </NavLink>
+          </div>
       </div>
     </div>
   );

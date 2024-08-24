e3e9bd473eeefce260419f298b0841bef3fbcbc4
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-08-24 09:57:51 -0700

    react

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -68,7 +68,7 @@ const Products = ({ data, title }) => {
     );
   });
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-5">
       <h3 className="text-2xl font-bold mb-10 mt-10">{title}</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
         {items}
